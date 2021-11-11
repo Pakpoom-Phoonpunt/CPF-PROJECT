6c7a30983e0b0d024652bf530457bc0dabc4c0e2
korrawee <korrawee.somyos@gmail.com>
2021-11-12 01:49:04 +0700

    Add mock-up user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+factory = Factory.create(name:"CPF")
+department = Department.create(name:"chicken", factory_id: factory.id)
+user= Account.create(username: "1", password: "1", factory_id: factory.id, department_id: department.id)
